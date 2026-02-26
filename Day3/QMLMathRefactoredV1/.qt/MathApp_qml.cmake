1317c9004bfa15cc3e39e8a0552fc7183cc4d913
Jeganathan Swaminathan <mail2jegan@gmail.com>
2026-02-26 12:55:47 +0530

    Removed binary files

--- Day3/QMLMathRefactoredV1/.qt/MathApp_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "MathApp")
-set(working_dir "/home/jegan/qt-feb-2026/Day3/QMLMathRefactoredV1")
-set(src_and_dest_list
-    "/home/jegan/qt-feb-2026/Day3/QMLMathRefactoredV1/main.qml"
-    "/home/jegan/qt-feb-2026/Day3/QMLMathRefactoredV1/App/main.qml"
-
-)
-set(timestamp_file "/home/jegan/qt-feb-2026/Day3/QMLMathRefactoredV1/.qt/MathApp_qml.txt")
